b125f3f501a902eb744b26eb8ccfb738780b6106
Anay Sinhal <91818144+anayy09@users.noreply.github.com>
2023-03-05 18:47:49 +0530

    Create NETFLIX.py

---
+++ Practice/NETFLIX.py
@@ -0,0 +1,8 @@
+# NETFLIX
+
+for test in range(int(input())):
+    a,b,c,x = map(int, input().split())
+    if a + b >= x or b + c >= x or a + c >= x:
+        print("YES")
+    else:
+        print("NO")
